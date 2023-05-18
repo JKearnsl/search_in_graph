fc0e77369d56fce174a84780d9b762a9a7b80fd7
jkearnsl <boyarshinovn@gmail.com>
2023-05-18 17:54:15 +0000

    Оптимизирован алгоритм, теперь используются связи в стеке

--- src/model/sig_model/dfs.py
+++ src/model/sig_model/dfs.py
@@ -15,33 +15,31 @@ def dfs(graph: list[tuple], start_vertex, search_value, graph_type: GraphType = 
 
     visited = []
     visited_path = []
-    stack = [start_vertex]
+    stack = [(None, start_vertex)]
     is_found = False
 
     start_time = time.time()
-    # TODO: сохранять в стеке связи а не вершины
     while stack:
-        vertex = stack.pop()
-        if vertex not in visited:
-            for x in graph:
-                if x[1] == vertex and x[0] in visited and x[1] not in visited:
-                    visited_path.append(x)
-                elif graph_type == GraphType.UNDIRECTED and x[0] == vertex and x[1] in visited and x[0] not in visited:
-                    visited_path.append(x)
+        pre_vertex, cur_vertex = stack.pop()
+        if cur_vertex not in visited:
+            visited.append(cur_vertex)
 
-            visited.append(vertex)
+            if pre_vertex is not None:
+                visited_path.append((pre_vertex, cur_vertex))
+
             if graph_type == GraphType.DIRECTED:
-                directions = [x[1] for x in graph if x[0] == vertex]
+                directions = [x[1] for x in graph if x[0] == cur_vertex]
             else:
                 directions = []
                 for x in graph:
-                    if x[0] == vertex:
+                    if x[0] == cur_vertex:
                         directions.append(x[1])
-                    elif x[1] == vertex and x[0] not in visited:
+                    elif x[1] == cur_vertex and x[0] not in visited:
                         directions.append(x[0])
-            stack.extend(directions)
 
-        if vertex == search_value:
+            stack.extend([(cur_vertex, x) for x in directions])
+
+        if cur_vertex == search_value:
             is_found = True
             break
     end_time = time.time()
