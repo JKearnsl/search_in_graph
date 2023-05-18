fdc03deeca2452a0b610efb54bf8d77ef1646d2c
jkearnsl <boyarshinovn@gmail.com>
2023-05-18 16:33:39 +0000

    более правильный visited_path

--- src/model/sig_model/dfs.py
+++ src/model/sig_model/dfs.py
@@ -1,4 +1,3 @@
-import random
 import time
 
 from src.model.enum.graph import GraphType
@@ -14,22 +13,25 @@ def dfs(graph: list[tuple], start_vertex, search_value, graph_type: GraphType = 
     :return:
     """
 
-    visited = set()
-    path = []
+    visited = []
+    visited_path = []
     stack = [start_vertex]
     is_found = False
 
     start_time = time.time()
-    prev_vertex = None
+    # TODO: сохранять в стеке связи а не вершины
     while stack:
         vertex = stack.pop()
         if vertex not in visited:
-            visited.add(vertex)
-            if prev_vertex is not None:
-                path.append((prev_vertex, vertex))
-            prev_vertex = vertex
+            for x in graph:
+                if x[1] == vertex and x[0] in visited and x[1] not in visited:
+                    visited_path.append(x)
+                elif graph_type == GraphType.UNDIRECTED and x[0] == vertex and x[1] in visited and x[0] not in visited:
+                    visited_path.append(x)
+
+            visited.append(vertex)
             if graph_type == GraphType.DIRECTED:
-                stack.extend([x[1] for x in graph if x[0] == vertex])
+                directions = [x[1] for x in graph if x[0] == vertex]
             else:
                 directions = []
                 for x in graph:
@@ -37,12 +39,11 @@ def dfs(graph: list[tuple], start_vertex, search_value, graph_type: GraphType = 
                         directions.append(x[1])
                     elif x[1] == vertex and x[0] not in visited:
                         directions.append(x[0])
-                random.shuffle(directions)
-                stack.extend(directions)
+            stack.extend(directions)
 
         if vertex == search_value:
             is_found = True
             break
     end_time = time.time()
 
-    return is_found, (end_time - start_time) * 1000, path
+    return is_found, (end_time - start_time) * 1000, visited_path
