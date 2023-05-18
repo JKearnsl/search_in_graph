eed2841ac201e9a33cab858639aad1bbf803b8d1
jkearnsl <boyarshinovn@gmail.com>
2023-05-18 16:34:12 +0000

    возможность выбрать способ отображения графа

--- src/model/enum/graph.py
+++ src/model/enum/graph.py
@@ -4,3 +4,9 @@ from enum import Enum
 class GraphType(Enum):
     DIRECTED = "ОРГ"
     UNDIRECTED = "НеОРГ"
+
+
+class ShowAs(Enum):
+    FULL_GRAPH = "Исходный граф"
+    VISITED_PATH = "Пройденный путь"
+    PATH = "Идеальный путь"
